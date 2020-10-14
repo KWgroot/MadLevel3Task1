968a328939993108aa6ef9d597060e72b9e5ad63
Koen Groot <kwgroot@gmail.com>
2020-10-14 13:44:46 +0200

    Navigation between pages done.

--- app/src/main/java/com/example/madlevel3task1/StartFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/StartFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_start.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,10 +18,15 @@ class StartFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_start, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        btnStart.setOnClickListener{
+            findNavController().navigate(
+                R.id.action_FirstFragment_to_SecondFragment
+            )
+        }
     }
 }
